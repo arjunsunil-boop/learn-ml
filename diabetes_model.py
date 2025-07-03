87e2c896d4ddafcb3c4b4de33396eae6711b8613
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2025-07-04 03:37:56 +0530

    decision tree

--- diabetes_model.py
+++ diabetes_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 df = pd.read_csv('Datasets\diabetes.csv')
 print(df.head())
@@ -12,17 +13,14 @@ y = df["Outcome"]
 
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
 
-knn_model = KNeighborsClassifier(n_neighbors=3)
+# knn_model = KNeighborsClassifier(n_neighbors=3)
 
-knn_model.fit(x_train,y_train)
+# knn_model.fit(x_train,y_train)
 
-pred_values = knn_model.predict(x_test)
+decision_tree = DecisionTreeClassifier()
+decision_tree.fit(x_train,y_train)
+
+pred_values = decision_tree.predict(x_test)
 
 acc = accuracy_score(y_test,pred_values)
-print(acc)
-
-# test_value = pd.DataFrame([[2,109,92,0,0,42.7,0.845,54]])
-
-# result = knn_model.predict(test_value)
-
-# print(result)
+print("Accuracy is ",acc*100)
